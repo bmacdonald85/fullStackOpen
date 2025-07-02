0fc7a2dbd6250649e43f39927f886c8f954aaf4c
bmacdonald85 <bmac85@gmail.com>
2025-07-02 10:53:42 -0400

    1.2 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,18 +4,20 @@ const Header = ({ course }) => {
 
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
-    <>
-      <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-    </>
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
   );
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
 }
 
 const Total = ({ exercises1, exercises2, exercises3 }) => {
