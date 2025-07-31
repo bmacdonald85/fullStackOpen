716661a1fca703521d4aeee4cec0dd31de531844
bmacdonald85 <bmac85@gmail.com>
2025-07-31 14:58:26 -0400

    3.15-3.18 complete

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -17,59 +17,40 @@ app.use(morgan(function (tokens, req, res) {
 }))
 app.use(express.static('dist'))
 
-// let persons = [
-//     { 
-//       id: "1",
-//       name: "Arto Hellas", 
-//       number: "040-123456"
-//     },
-//     { 
-//       id: "2",
-//       name: "Ada Lovelace", 
-//       number: "39-44-5323523"
-//     },
-//     { 
-//       id: "3",
-//       name: "Dan Abramov", 
-//       number: "12-43-234345"
-//     },
-//     { 
-//       id: "4",
-//       name: "Mary Poppendieck", 
-//       number: "39-23-6423122"
-//     }
-// ]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(person => {
     response.json(person)
   })
 })
 
-app.get('/info', (request, response) => {
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({}).then(count => {
+    const info = `<p>Phonebook has info for ${count} people</p>
                   <p>${new Date()}</p>`
     response.send(info)
+  }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    Person.findById(id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    }).catch(error => {
-        console.error('Error fetching person:', error)
-        response.status(500).send({ error: 'Internal server error' })
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id).then(person => {
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id).then(result => {
+    if (result) {
+        response.status(204).end()
+    } else {
+        response.status(404).send({ error: 'Person not found' })
+    }
+  }).catch(error => next(error))
 })
 
 const generateId = () => {
@@ -79,24 +60,59 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'name or number missing' })
-    }
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    }).catch(error => {
-        console.error('Error saving person:', error)
-        response.status(500).send({ error: 'Internal server error' })
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then(savedPerson => {
+      response.json(savedPerson)
+  }).catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  const person = {
+      name: body.name,
+      number: body.number,
+  }
+
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
